c2735ae98d3aac885b0d4b1548eb25e4c5de379f
Nick Bansal <extreme_wired@msn.com>
2018-10-14 10:52:18 +0100

    listen.js

--- listen.js
+++ listen.js
@@ -2,6 +2,6 @@ const app = require('./app')
 const port = 8000;
 // 'mongodb://Bansal321:75J0tryG!@ds131753.mlab.com:31753/ncnews'
 
-app.listen(proces.env.PORT, process.env.MONGO_URI, () => {
-    console.log(`Server running on port: ${port}`)
+app.listen(process.env.PORT, process.env.MONGO_URI, () => {
+    console.log(`Server running on port: ${process.env.PORT}`)
 })

511544bc807142325d0a641441a032b4dc8f1a4f
Nick Bansal <extreme_wired@msn.com>
2018-10-14 10:54:23 +0100

    DB_URL

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { DB_URL } = require('./config')
+const DB_URL = 'mongodb://Bansal321:75J0tryG!@ds131753.mlab.com:31753/ncnews'
 const APIrouter = require('./routes/APIrouter');
 const bodyParser = require('body-parser');
 const { handle404, handle400, handle500 } = require('./error-handlers')
 
-mongoose.connect('mongodb://Bansal321:75J0tryG!@ds131753.mlab.com:31753/ncnews', { useNewUrlParser: true })
+mongoose.connect(DB_URL, { useNewUrlParser: true })
   .then(console.log(`Database is running on ${DB_URL}`))
 
 app.use(bodyParser.json())

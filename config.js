a07de1a83072368584c11503c0cb8d9e8698d01f
Nick Bansal <extreme_wired@msn.com>
2018-10-14 17:30:52 +0100

    Heroku Push

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const config = {
     },
     test: {
         DB_URL: 'mongodb://localhost:27017/nc_news_test'
-    }
+    },
+    production: 'mongodb://NickBansal:75J0tryG@ds131753.mlab.com:31753/ncnews'
 }
 
 module.exports = config[env];

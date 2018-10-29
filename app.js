164819eee663361ff6ef6c781c3e73cbd0267a68
Nick Bansal <extreme_wired@msn.com>
2018-10-29 15:31:05 +0000

    CORS

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const { DB_URL } = require('./config')
 const APIrouter = require('./routes/APIrouter');
 const bodyParser = require('body-parser');
 const { handle404, handle400, handle500 } = require('./error-handlers')
+const cors = require('cors')
 
 mongoose.connect(DB_URL, { useNewUrlParser: true })
   .then(console.log(`Database is running on ${DB_URL}`))
 
 app.set('view engine', 'ejs')
-
+app.use(cors())
 app.use(express.static('public'), bodyParser.json())
 app.use('/api', APIrouter)
 app.use('/*', (req, res, next) => next({ status: 404, msg: `${req.originalUrl} does not exist` }))

c406d334e4c9720d127d2d83e9461f6a16550b52
Nick Bansal <extreme_wired@msn.com>
2018-11-03 21:16:13 +0000

    send articles by username

--- controllers/userCons.js
+++ controllers/userCons.js
@@ -15,3 +15,22 @@ exports.sendUserByUsername = (req, res, next) => {
     })
     .catch(next)
 }
+
+exports.sendAllArticlesByUser = (req, res, next) => {
+    const { username } = req.params
+    Articles.find({ username })
+    .populate('created_by')
+    .then(articles => {
+        res.send(articles)
+    })
+    .catch(next)
+}
+
+// exports.sendCommentsByUsername = (req, res, next) => {
+//     const { username } = req.params
+//     Articles.find({ username })
+//     .then(articles => {
+//         res.send(articles)
+//     })
+//     .catch(next)
+// }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,17 @@
 const userRouter = require('express').Router();
-const { sendAllUsers, sendUserByUsername } = require('../controllers/userCons')
+const { 
+    sendAllUsers, 
+    sendUserByUsername, 
+    sendAllArticlesByUser, 
+    sendCommentsByUsername 
+} = require('../controllers/userCons')
 
 userRouter.get('/', sendAllUsers)
 
 userRouter.get('/:username', sendUserByUsername)
+
+userRouter.get('/:username/articles', sendAllArticlesByUser)
+
+userRouter.get('/:username/comments', sendCommentsByUsername)
 
 module.exports = userRouter;

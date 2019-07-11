03ccd3234c6cfa2abc8ff871442b17d5a14fe56a
Nick Bansal <extreme_wired@msn.com>
2019-07-11 10:12:46 +0100

    re seeded the database

--- seed/seed-dev.js
+++ seed/seed-dev.js
@@ -1,10 +1,11 @@
+process.env.NODE_ENV = 'production'
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
-const DB_URL  = 'mongodb://Nick:bansal321@ds131753.mlab.com:31753/ncnews'
+const { DB_URL } = require('../config')
 const { articles, comments, topics, users } = require('./devData');
 
 
-mongoose.connect(DB_URL, {useNewUrlParser: true})
+mongoose.connect(DB_URL, { useNewUrlParser: true })
     .then(() => seedDB(topics, users, articles, comments))
     .then(() => mongoose.disconnect())
     .then(console.log('Finished'))
